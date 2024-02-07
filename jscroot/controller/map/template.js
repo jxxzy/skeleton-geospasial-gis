0c3d73b2a65d24511c56d9dba3bd096202d8b9eb
Jxxzyshn <71040617+jxxzy@users.noreply.github.com>
2024-02-07 15:20:10 +0700

    .
    .

---
+++ jscroot/controller/map/template.js
@@ -0,0 +1,16 @@
+export let xxx = "https://us-central1-exemplary-oven-401904.cloudfunctions.net/function-1";
+export let aaa = "https://asia-southeast2-exemplary-oven-401904.cloudfunctions.net/tambahpoint";
+export let bbb = "https://asia-southeast2-exemplary-oven-401904.cloudfunctions.net/tambahpolyline";
+export let ccc = "https://asia-southeast2-exemplary-oven-401904.cloudfunctions.net/tambahpolygon";
+export let qqq = "https://asia-southeast2-befous.cloudfunctions.net/Befous/PostGeoIntersects";
+export let www = "https://asia-southeast2-befous.cloudfunctions.net/Befous/PostGeoWithin";
+export let eee = "https://asia-southeast2-befous.cloudfunctions.net/Befous/PostNear";
+export let rrr = "https://asia-southeast2-befous.cloudfunctions.net/Befous/PostNearSphere";
+export let ttt = "https://asia-southeast2-befous.cloudfunctions.net/Befous/PostBox";
+export let yyy = "https://asia-southeast2-befous.cloudfunctions.net/Befous/PostCenter";
+export let uuu = "https://asia-southeast2-befous.cloudfunctions.net/Befous/PostCenterSphere";
+
+export let clickpopup = `
+Coordinate  : #COORDINATE#<br>
+Type  : #TYPE#<br>
+`
